1cdb11bca6ec586ac07031707044b6ec7f1d6322
Sebastian <seb154@live.com>
2023-10-19 22:18:18 -0400

    modifies index.js and package-lock.json

---
+++ index.js
@@ -0,0 +1,12 @@
+function receivesAFunction(callback) {
+callback(); 
+     }
+
+function returnsANamedFunction(callback) {
+return returnsANamedFunction
+}
+function returnsAnAnonymousFunction() {
+    return function () {
+
+    };
+}
